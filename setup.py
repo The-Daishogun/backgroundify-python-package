ac2e7e2c7415d8fe1bb5edbef89cf7870d879102
Mohammadreza Varasteh <varastehmr@gmail.com>
2020-11-21 21:34:40 +0330

    UPDATE version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import version
 
 setup(
     name="backgroundify",
-    version="0.2",
+    version="0.3",
     description="Tools to change your background using bing image of the day",
     url="https://git.pe42.ir/MRV/python-bing-background",
     author="Mohammadreza Varasteh",
